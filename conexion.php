7ccd0a836d58f54c2de961192e76539c65582fa5
Josué G <josue.1013e@gmail.com>
2018-10-23 21:28:19 -0500

    contiene la conexión a la DB

--- conexion.php
+++ conexion.php
@@ -1,12 +1,12 @@
 <?php
 //datos de conexion
     function conectarse(){
-        $enlace = mysqli_connect("localhost", "josue", "123456789");
+        $enlace = mysqli_connect("localhost", "josue", "123456789");//servidor, user, password
         if(!($enlace)){
-            echo "Error al conectar a la base de datos.";
+            echo "Error al conectar al servidor.";
             exit();
         }
-        if(!mysqli_select_db($enlace, "clientes")){
+        if(!mysqli_select_db($enlace, "clientes")){//seleccionas la base de datos
             echo"Error al seleccionar la base de datos.";
             exit();
         }
