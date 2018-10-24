a43f0ed6991faadd0f7369296a9b8a9dc8e30804
Josué G <josue.1013e@gmail.com>
2018-10-24 00:58:36 -0500

    explica como se realiza la eliminacion de un registro

--- eliminar.php
+++ eliminar.php
@@ -2,7 +2,7 @@
     include("conexion.php");
     $enlace = conectarse();
 
-    //si presiona el boton eliminar
+    //al marcar el checkbox toma el id del registro de la DB lo pone en IdCliente y al presionar eliminar ejecuta el query
     if(@$_POST['btnEliminar']){
         foreach($_POST['IdCliente'] as $id){
             mysqli_query($enlace, 'DELETE FROM directorio WHERE id = '. $id);
